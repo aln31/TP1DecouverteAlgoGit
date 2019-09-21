868d3e13d602447e9a76cbf67b69d5a60bcce838
arnaud <a.lenedelec@free.fr>
2019-09-21 20:43:42 +0200

    ajout de la boucle de test et du compteur de coups

--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@ using namespace std;
 
 int main(){
 
+	srand(time(NULL));
+	int alea=rand()%100 + 1;
 	int nombre=0;
+	int essais=0;
 	//afficher un txt à lécran
-	cout << "entrez un nombre" << endl;
+	cout << "entrez un nombre entre 1 et 100" << endl;
 	//lire le nombre tapé et l'enregistrer dans la variable nombre
-	cin >> nombre;
 
-	//affiche à l'écran le nombre saisi
-	cout << "Vous avez saisi : " << nombre << endl;
+		while (alea!=nombre)
+			{
+			essais++;
+			cin >> nombre;
+				if (alea<nombre)
+					cout << "Trop grand" << endl;
+				else if (alea>nombre)
+					cout << "Trop petit" << endl;
+			}
+	cout << "Felicitation vous avez trouvé le nombre mystère (" << nombre << ") en : " << essais << " essais !" << endl;
 
 	//le prog se termine normalement en renvoyant 0
 	return 0;
